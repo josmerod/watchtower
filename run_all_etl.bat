80d7d5de2ab526ce87f3226ea840f78401bdbb39
josmerod <josemanuel.rodriguezmejias.ext@bayer.com>
2025-04-28 03:40:53 +0200

    Added meneame as a source Added podcasts rss as a source

--- run_all_etl.bat
+++ run_all_etl.bat
@@ -12,10 +12,13 @@ start /B python3 src/etl/news/news_get_genai_medium.py
 start /B python3 src/etl/news/news_get_bensbites.py
 start /B python3 src/etl/news/news_get_planesvalencia.py
 start /B python3 src/etl/news/news_get_gooddevs.py
+start /B python3 src/etl/news/news_get_meneame.py
+start /B python3 src/etl/news/news_get_podcasts.py
 start /B python3 src/etl/goldigging/goldigging_youtube_posts.py
 start /B python3 src/watchers/ms_skills_watcher.py --once
 start /B python3 src/etl/goldigging/goldigging_udemy_courses.py
 start /B python3 src/etl/goldigging/goldigging_coursera_courses.py
+
 
 echo All ETL processes started in parallel
 echo Check logs for individual process status 
